f351ec216d5f9be78099171e2ff7eff2e258e790
Jomar Cerrado <77486911+DevJomar2793@users.noreply.github.com>
2024-04-04 14:41:15 +0800

    updated design grading system

---
+++ GradingSystem/public/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+            <title>Grading System</title>
+    </head>
+    <body>
+        <div class="w-full h-screen bg-cover flex align-center justify-center " style="background-image: url('../design/Hallway.jpg')">
+            <div class="w-[300px] h-[500px] border-2 self-center bg-slate/70 backdrop-blur-xl bg border-gray-500" >
+                <div class="p-2 text-center text-lg bg-sky-500/75">
+                    <h1 class="text-white font-semibold">Grading System</h1>
+                </div>
+                <div class="m-2 my-2 py-8 space-y-4">
+                    <form class="grid grid-cols-2 gap-4" action="">
+                        <h3 class="text-center text-sky-700 font-black text-2xl">English:</h3>
+                        <input class="w-[100px] " type="number" name="" id="">
+                    </form>
+                    <form class="grid grid-cols-2 gap-4" action="">
+                        <h3 class="text-center text-sky-700 font-black text-2xl">Math:</h3>
+                        <input class="w-[100px] " type="number" name="" id="">
+                    </form>
+                    <form class="grid grid-cols-2 gap-4" action="">
+                        <h3 class="text-center text-sky-700 font-black text-2xl">Physics:</h3>
+                        <input class="w-[100px] " type="number" name="" id="">
+                    </form>
+                    <form class="grid grid-cols-2 gap-4" action="">
+                        <h3 class="text-center text-sky-700 font-black text-2xl">Chemistry:</h3>
+                        <input class="w-[100px] " type="number" name="" id="">
+                    </form>
+                    <form class="grid grid-cols-2 gap-4" action="">
+                        <h3 class="text-center text-sky-700 font-black text-2xl">Computer:</h3>
+                        <input class="w-[100px] " type="number" name="" id="">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ GradingSystem/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,php}"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        btnColor: '#88ceeb'
+      }
+    },
+  },
+  plugins: [],
+}
+
